b91f6e9b31cd543c8838fe1b2de47942f3bcacf9
Sergei Shipilov <sergeishipilou@yandex.by>
2020-09-29 21:54:28 +0300

    feat: add task solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) return n.toString().split('').slice(1, n.length).reverse().join('');
+  else return n.toString().split('').reverse().join('');
 }
+
